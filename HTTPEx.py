93d8450cd419bde6c7df34570c9b1fc0938aea90
Andrew <andrew.sampino1@gmail.com>
2023-03-04 15:56:02 -0500

    query strings

--- HTTPEx.py
+++ HTTPEx.py
@@ -1,7 +1,9 @@
 import requests
-response = requests.get('https://api.github.com')
+response = requests.get('https://api.github.com', params={'q': 'requests+language:python'},)
 
 if response:
+    for thing in response.json():
+        print(thing)
     print("Get successful")
 else:
     print("Get failure")
